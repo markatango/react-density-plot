22cdff3a81f4431a6e0d3c1224d7894cdcba610a
Mark Rodighiero <markatango@gmail.com>
2023-02-03 17:27:59 -0800

    cleaned up comment

--- src/lib/canvasjs.react.js
+++ src/lib/canvasjs.react.js
@@ -3,7 +3,9 @@ CanvasJS React Charts - https://canvasjs.com/
 Copyright 2021 fenopix
 
 --------------------- License Information --------------------
-CanvasJS is a commercial product which requires purchase of license. Without a commercial license you can use it for evaluation purposes for upto 30 days. Please refer to the following link for further details.
+CanvasJS is a commercial product which requires purchase of license. 
+Without a commercial license you can use it for evaluation purposes for upto 30 days. 
+Please refer to the following link for further details.
 https://canvasjs.com/license/
 
 */
