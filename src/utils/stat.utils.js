832d42ff1460e2c15eeb273d967d68e5d2036033
Mark Rodighiero <markatango@gmail.com>
2023-02-03 17:25:22 -0800

    commented out logging

--- src/utils/stat.utils.js
+++ src/utils/stat.utils.js
@@ -4,8 +4,8 @@ const randn_bm = () => {
 	while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
 	while(v === 0) v = Math.random();
 	let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-	num = num / 10.0 + 0.5; // Translate to 0 -> 1
-	if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
+	// num = num / 10.0 + 0.5; // Translate to 0 -> 1 and center at 0.5
+	// if (num > 1 || num < 0) return randn_bm() // resample between 0 and 1
 	return num
 }
 
@@ -18,44 +18,44 @@ export const getData = (dataType, length) => {
 }
 
 export const genNormalData = (numPts=30) => {
-	console.log(`genNormalData(${numPts})`)
+	// console.log(`genNormalData(${numPts})`)
 	const data = []
 	const numPoints = numPts;
 	for (let i=0;i<numPoints;++i){
 		data.push({x: i, y: randn_bm()})
 	}
-	console.log(`data[0]: ${JSON.stringify(data[0])}`)
+	// console.log(`data[0]: ${JSON.stringify(data[0])}`)
 	return data;
 }
 
 export const genUniformData = (numPts=30) => {
-	console.log(`genUniformData(${numPts})`)
+	// console.log(`genUniformData(${numPts})`)
 	const data = []
 	const numPoints = numPts;
 	for (let i=0;i<numPoints;++i){
 		data.push({x: i, y: Math.random()})
 	}
-	console.log(`data[0]: ${JSON.stringify(data[0])}`)
+	// console.log(`data[0]: ${JSON.stringify(data[0])}`)
 	return data;
 }
 
 export const genPoissonData = (numPts=30) => {
-	//console.log(`genPoissonData(${numPts})`)
+	// console.log(`genPoissonData(${numPts})`)
 	const data = []
 	const numPoints = numPts;
 	const L = Math.exp(-7)
-	console.log(L)
+	// console.log(L)
 	for (let i=0;i<numPoints;++i){
 		let p = 1
 		let k = 0
 		do {
 			k += 1
 			p *= Math.random()
-			console.log(p)
+			// console.log(p)
 		} while (p > L);
 		data.push({x: i, y: k-1 })
 	}
-	console.log(`data[0]: ${JSON.stringify(data[0])}`)
+	// console.log(`data[0]: ${JSON.stringify(data[0])}`)
 	return data;
 }
 
